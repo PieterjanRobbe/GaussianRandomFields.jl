95b6370f4f9a9cc09e5af579ce3077744b031b7e
PieterjanR <pieterjan.robbe@cs.kuleuven.be>
2021-09-14 16:08:47 -0700

    Fixed some tests

--- test/test_show.jl
+++ test/test_show.jl
@@ -2,16 +2,27 @@
 
 @info "Testing display commands"
 
-@show Matern(1,1)
-@show Exponential(1)
-@show SquaredExponential(1)
-@show AnisotropicExponential([1000 0; 0 1000])
-@show CovarianceFunction(2,Matern(1.0,2))
-@show Cholesky()
-@show Spectral()
-@show KarhunenLoeve(10)
-@show CirculantEmbedding()
-@show GaussianRandomField(CovarianceFunction(1,Exponential(1)),Cholesky(),0:0.1:1)
-@show SeparableCovarianceFunction(Exponential(1),Exponential(1))
+str = string(Matern(1,1))
+@test str isa String
+str = string(Exponential(1))
+@test str isa String
+str = string(SquaredExponential(1))
+@test str isa String
+str = string(AnisotropicExponential([1000 0; 0 1000]))
+@test str isa String
+str = string(CovarianceFunction(2,Matern(1.0,2)))
+@test str isa String
+str = string(Cholesky())
+@test str isa String
+str = string(Spectral())
+@test str isa String
+str = string(KarhunenLoeve(10))
+@test str isa String
+str = string(CirculantEmbedding())
+@test str isa String
+str = string(GaussianRandomField(CovarianceFunction(1,Exponential(1)),Cholesky(),0:0.1:1))
+@test str isa String
+str = string(SeparableCovarianceFunction(Exponential(1),Exponential(1)))
+@test str isa String
 
 @info " Done testing display commands"
