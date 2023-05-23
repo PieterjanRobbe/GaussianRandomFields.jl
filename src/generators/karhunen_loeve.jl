d331e0ad1aa72e09ce54fd82e6559f30c55871e9
PieterjanR <pieterjan.robbe@cs.kuleuven.be>
2023-05-23 20:52:53 +0200

    Updated doctest for rel_error

--- src/generators/karhunen_loeve.jl
+++ src/generators/karhunen_loeve.jl
@@ -149,8 +149,8 @@ julia> pts = range(0, stop=1, length=51)
 julia> grf = GaussianRandomField(cov, KarhunenLoeve(300), pts, pts)
 Gaussian random field with 2d Matérn covariance function (λ=0.3, ν=1.0, σ=1.0, p=2.0) on a 51×51 structured grid, using a KL expansion with 300 terms
 
-julia> rel_error(grf)
-0.00046070730242930846
+julia> @printf "%.8f" rel_error(grf)
+0.00046071
 
 ```
 """
